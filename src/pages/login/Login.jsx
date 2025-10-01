d9abd86cebffb628f129377aa83d337383e642f5
Altin Percuku <altinpercuku05@gmail.com>
2025-10-01 17:18:20 +0200

    Created Login/Register Toggle Logic

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css"
 import assets from "../../../src/assets/assets.js"
 
 
 const Login = () => {
+    const [currState, setCurrState] = useState("Sign Up")
   return (
     <div className='login'>
         <img src={assets.logo_big} className='logo' alt="" />
         <form className='login-form'>
-            <h2>Sign Up</h2>
-            <input type="text" placeholder='Username' className='form-input' required />
+            <h2>{currState}</h2>
+            {currState === "Sign Up" ? <input type="text" placeholder='Username' className='form-input' required /> : null}
             <input type="email" placeholder='Email Address'  className='form-input' required />
             <input type="text" placeholder='Password' className='form-input' required />
-            <button type='submit' >Sign up</button>
+            <button type='submit' >{currState}</button>
             <div className="login-term">
                 <input type='checkbox'/>
                 <p>Agree to the terms of use and privacy policy</p>
             </div>
             <div className="login-forgot">
-                <p className="login-toggle">
-                    Already have an account? <span>Click here</span> 
-                </p>
+                { currState === "Sign Up" ? <p className="login-toggle">
+                    Already have and account? <span onClick={() => setCurrState("Log In")}>Click here</span>
+                </p> :
+                    <p className="login-toggle">
+                        Don't have an account? <span onClick={() => setCurrState("Sign Up")}>Click here</span>
+                    </p>
+                }
             </div>
         </form>
     </div>
