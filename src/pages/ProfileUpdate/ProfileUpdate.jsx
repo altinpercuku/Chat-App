04710c6d37e64cec7ecb1861f08920659501b138
Altin Percuku <altinpercuku05@gmail.com>
2025-10-02 17:30:19 +0200

    Created profile details design view

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./ProfileUpdate.css"
+import assets from '../../assets/assets'
 
 
 const ProfileUpdate = () => {
+  const [profileImg, setProfileImg] = useState(false)
+
   return (
-    <div>ProfileUpdate</div>
+    <div className='profile'>
+      <div className="profile-container">
+        <form>
+            <h3>Profile Details</h3>
+            <label htmlFor="avatar">
+              <input onChange={(e) => setProfileImg(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+              <img src={profileImg ? URL.createObjectURL(profileImg) : assets.avatar_icon} alt="Avatar Icon" />
+              Upload Profile Image
+            </label>
+            <input type="text" placeholder='Your name'  required  />
+            <textarea placeholder='Write Profile Bio' required></textarea>
+            <button>Apply Changes</button>
+        </form>
+        <img src={profileImg ? URL.createObjectURL(profileImg) : assets.avatar_icon} alt="Logo" className="profile-pic" />
+      </div>
+    </div>
   )
 }
 
